0652d5d4f44393d3dd3265cd36a3ea7e92aa938b
mw3981 <mw3981@nyu.edu>
2022-02-06 04:32:06 -0500

    Updating layout pattern

--- week2/src/main.ts
+++ week2/src/main.ts
@@ -2,9 +2,7 @@ import * as PIXI from "pixi.js";
 
 const main = async () => {
   // Actual app
-  let app = new PIXI.Application({
-    backgroundColor: 0xe3efdc,
-  });
+  let app = new PIXI.Application();
 
   // Display application properly
   document.body.style.margin = "0";
@@ -16,38 +14,44 @@ const main = async () => {
 
   let draw = new PIXI.Graphics();
 
-  for (let y = 20; y < window.innerHeight; y += 108) {
-    for (let x = 30; x < window.innerWidth - 100; x += 100) {
-      draw.beginFill(0x533e42, 1);
-      draw.drawRect(x, y, 100, 15);
-    }
-  }
-
-  for (let y = 35; y < window.innerHeight - 50; y += 120) {
-    for (let x = 70; x < window.innerWidth - 130; x += 200) {
+  for (let y = 33; y < window.innerHeight - 150; y += 120) {
+    for (let x = 72; x < window.innerWidth - 130; x += 200) {
+      draw.lineStyle(4, 0x72bab6);
       draw.beginFill(0xffffaa, 1);
-      draw.drawRect(x, y, 95, 95);
+      draw.drawRoundedRect(x, y, 95, 95, 35);
+      draw.endFill();
     }
   }
 
-  for (let y = 60; y < window.innerHeight; y += 105) {
+  for (let y = 60; y < window.innerHeight - 50; y += 105) {
     for (let x = 180; x < window.innerWidth - 130; x += 200) {
+      draw.lineStyle(4, 0xffffaa);
       draw.beginFill(0x72bab6, 1);
       draw.drawRect(x, y, 75, 75);
     }
   }
 
-  for (let y = 80; y < window.innerHeight; y += 120) {
+  for (let y = 80; y < window.innerHeight - 50; y += 120) {
     for (let x = 120; x < window.innerWidth - 100; x += 200) {
+      draw.lineStyle(3, 0x72bab6);
       draw.beginFill(0x72bab6, 1);
       draw.drawCircle(x, y, 35);
     }
   }
 
-  for (let y = 97; y < window.innerHeight; y += 105) {
+  for (let y = 97; y < window.innerHeight - 50; y += 105) {
     for (let x = 217; x < window.innerWidth - 100; x += 200) {
       draw.beginFill(0xffffaa, 1);
       draw.drawCircle(x, y, 20);
+    }
+  }
+
+  for (let y = 50; y < window.innerHeight; y += 65) {
+    for (let x = 50; x < window.innerWidth; x += 60) {
+      draw.endFill();
+      draw.lineStyle(3, 0xffffff);
+      draw.drawRect(60, 60, y - 50, y - 40);
+      draw.drawRect(760, 70, y - 50, y - 40);
     }
   }
 
