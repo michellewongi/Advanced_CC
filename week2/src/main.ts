b69f95fb818fe9da60d6459c2533c30b3a15fde7
mw3981 <mw3981@nyu.edu>
2022-02-06 15:35:21 -0500

    Updating layout design

--- week2/src/main.ts
+++ week2/src/main.ts
@@ -10,12 +10,12 @@ const main = async () => {
   app.renderer.view.style.display = "block";
 
   // View size = windows
-  app.renderer.resize(window.innerWidth, window.innerHeight + 30);
+  app.renderer.resize(window.innerWidth, window.innerHeight);
 
   let draw = new PIXI.Graphics();
 
-  for (let y = 40; y < window.innerHeight; y += 120) {
-    for (let x = 40; x < window.innerWidth - 130; x += 200) {
+  for (let y = 0; y < window.innerHeight; y += 120) {
+    for (let x = 0; x < window.innerWidth - 100; x += 200) {
       draw.lineStyle(4, 0x72bab6);
       draw.beginFill(0xffffaa, 1);
       draw.drawRoundedRect(x, y, 95, 95, 35);
@@ -24,15 +24,15 @@ const main = async () => {
   }
 
   for (let y = 70; y < window.innerHeight; y += 105) {
-    for (let x = 150; x < window.innerWidth - 130; x += 200) {
+    for (let x = 115; x < window.innerWidth; x += 200) {
       draw.lineStyle(4, 0xffffaa);
       draw.beginFill(0x72bab6, 1);
       draw.drawRect(x, y, 75, 75);
     }
   }
 
-  for (let y = 88; y < window.innerHeight; y += 120) {
-    for (let x = 88; x < window.innerWidth - 100; x += 200) {
+  for (let y = 48; y < window.innerHeight; y += 120) {
+    for (let x = 48; x < window.innerWidth; x += 200) {
       draw.lineStyle(3, 0x72bab6);
       draw.beginFill(0x72bab6, 1);
       draw.drawCircle(x, y, 35);
@@ -40,18 +40,9 @@ const main = async () => {
   }
 
   for (let y = 108; y < window.innerHeight; y += 105) {
-    for (let x = 189; x < window.innerWidth - 100; x += 200) {
+    for (let x = 153; x < window.innerWidth; x += 200) {
       draw.beginFill(0xffffaa, 1);
       draw.drawCircle(x, y, 20);
-    }
-  }
-
-  for (let y = 50; y < window.innerHeight; y += 65) {
-    for (let x = 50; x < window.innerWidth; x += 60) {
-      draw.endFill();
-      draw.lineStyle(3, 0xffffff);
-      draw.drawRect(60, 60, y - 50, y - 40);
-      draw.drawRect(760, 70, y - 50, y - 40);
     }
   }
 
