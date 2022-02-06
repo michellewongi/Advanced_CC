7dc05c3c4a4df91dfec030f0a08608e6eaf03e54
mw3981 <mw3981@nyu.edu>
2022-02-06 04:47:37 -0500

    Updating layout

--- week2/src/main.ts
+++ week2/src/main.ts
@@ -10,11 +10,11 @@ const main = async () => {
   app.renderer.view.style.display = "block";
 
   // View size = windows
-  app.renderer.resize(window.innerWidth, window.innerHeight);
+  app.renderer.resize(window.innerWidth, window.innerHeight + 30);
 
   let draw = new PIXI.Graphics();
 
-  for (let y = 33; y < window.innerHeight - 150; y += 120) {
+  for (let y = 50; y < window.innerHeight - 50; y += 120) {
     for (let x = 72; x < window.innerWidth - 130; x += 200) {
       draw.lineStyle(4, 0x72bab6);
       draw.beginFill(0xffffaa, 1);
@@ -31,7 +31,7 @@ const main = async () => {
     }
   }
 
-  for (let y = 80; y < window.innerHeight - 50; y += 120) {
+  for (let y = 94; y < window.innerHeight; y += 120) {
     for (let x = 120; x < window.innerWidth - 100; x += 200) {
       draw.lineStyle(3, 0x72bab6);
       draw.beginFill(0x72bab6, 1);
@@ -39,15 +39,15 @@ const main = async () => {
     }
   }
 
-  for (let y = 97; y < window.innerHeight - 50; y += 105) {
+  for (let y = 97; y < window.innerHeight; y += 105) {
     for (let x = 217; x < window.innerWidth - 100; x += 200) {
       draw.beginFill(0xffffaa, 1);
       draw.drawCircle(x, y, 20);
     }
   }
 
-  for (let y = 50; y < window.innerHeight - 100; y += 65) {
-    for (let x = 50; x < window.innerWidth - 100; x += 60) {
+  for (let y = 50; y < window.innerHeight; y += 65) {
+    for (let x = 50; x < window.innerWidth; x += 60) {
       draw.endFill();
       draw.lineStyle(3, 0xffffff);
       draw.drawRect(60, 60, y - 50, y - 40);
