b960b5a38b3415078b039e7c453b8ea2d2536f63
mw3981 <mw3981@nyu.edu>
2022-03-06 14:16:59 -0500

    adding midterm

---
+++ midterm/README.md
@@ -0,0 +1,13 @@
+# Advanced Creative Coding Midterm Project
+
+## Overview
+
+For my midterm, I created a landscape scene and users can navigate around the scene through a peep-hole. When the user clicks down anywhere on the screen, the whole scene is revealed with a frame instead of a peep-hole. I applied a displacement filter to the clouds to make them look like water ripples. I also applied a glitch filter to the moon sprite.
+
+## Initial Concept
+
+My original idea was to create a scene of a moon and the ocean. I ended up adding more elements like the peep-hole/frame and filters.
+
+## Further Development
+
+I want to add a screenshot functionality that will only take a screenshot of the view within the polaroid film.
---
+++ midterm/src/main.ts
@@ -0,0 +1,279 @@
+import * as PIXI from "pixi.js";
+import gsap from "gsap";
+import { GlitchFilter } from "@pixi/filter-glitch";
+
+let wavesCon = new PIXI.Container();
+let cloudsCon = new PIXI.Container();
+let moonCon = new PIXI.Container();
+let frameCon = new PIXI.Container();
+let t1 = gsap.timeline();
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+const load = (app: PIXI.Application) => {
+  return new Promise<void>((resolve) => {
+    app.loader
+      .add("filter", "assets/filter.png")
+      .add("moon", "assets/moon.png")
+      .add("cloud", "assets/cloud.png")
+      .add("bg", "assets/bg.jpg")
+      .add("frame", "assets/frame.png")
+      .load(() => {
+        resolve();
+      });
+  });
+};
+
+const main = async () => {
+  // Actual app
+  let app = new PIXI.Application({
+    antialias: true,
+  });
+  // Display application properly
+  document.body.style.margin = "0";
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+
+  // View size = windows
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+
+  // Load assets
+  await load(app);
+
+  // Handle window resizing
+  window.addEventListener("resize", (_e) => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+  });
+
+  scene();
+  app.stage.addChild(moonCon);
+  app.stage.addChild(cloudsCon);
+  app.stage.addChild(wavesCon);
+  app.stage.addChild(frameCon);
+
+  // scene one
+  function scene() {
+    // add frames
+    const frame = PIXI.Sprite.from("assets/frame.png");
+    frame.scale.set(0.8);
+    frame.anchor.set(0.5);
+    frame.x = width / 2;
+    frame.y = height / 2;
+
+    const shot = new PIXI.Graphics();
+    shot.beginFill(0x000000);
+    shot.drawCircle(0, 0, 100);
+    frameCon.addChild(shot);
+    app.stage.interactive = true;
+    app.stage.mask = shot;
+    app.stage.on("pointermove", moveFrame);
+    app.stage.on("pointerdown", switchFrames);
+
+    // function for switching view frames
+    function switchFrames() {
+      if (frameCon.children.includes(shot)) {
+        frameCon.removeChild(shot);
+        frameCon.addChild(frame);
+        app.stage.mask = null;
+      } else {
+        frameCon.removeChild(frame);
+        frameCon.addChild(shot);
+        app.stage.mask = shot;
+      }
+    }
+
+    // function to move frame with mouse
+    function moveFrame(e: any) {
+      let pos = e.data.global;
+      shot.x = pos.x;
+      shot.y = pos.y;
+      frame.x = pos.x;
+      frame.y = pos.y;
+    }
+
+    // add background
+    const bg = PIXI.Sprite.from("assets/bg.jpg");
+    bg.anchor.set(0.5);
+    bg.position.set(app.screen.width / 2, app.screen.height / 2);
+    bg.scale.set(0.4);
+    app.stage.addChild(bg);
+
+    // add filters
+    const filterTexture = PIXI.Sprite.from("assets/filter.png");
+    moonCon.addChild(filterTexture);
+    filterTexture.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    const displacement = new PIXI.filters.DisplacementFilter(filterTexture);
+    const glitch = new GlitchFilter();
+    glitch.slices = 30;
+    glitch.fillMode = 1;
+    moonCon.filters = [glitch];
+
+    // timer for displacement filter
+    app.ticker.add(function () {
+      filterTexture.x += 3;
+      filterTexture.y += 3;
+    });
+
+    // add moon
+    let moon = PIXI.Sprite.from("assets/moon.png");
+    moon.scale.set(0.3);
+    moon.x = width / 2 - 300;
+    moon.y = height / 2 + 500;
+    moonCon.addChild(moon);
+
+    gsap.to(moon, {
+      y: 140,
+      duration: 4,
+    });
+
+    // add clouds
+    let cloud = PIXI.Sprite.from("assets/cloud.png");
+    cloud.scale.set(0.2);
+    cloud.x = width / 2 - 720;
+    cloud.y = height / 2 - 320;
+    cloudsCon.addChild(cloud);
+
+    let cloud2 = PIXI.Sprite.from("assets/cloud.png");
+    cloud2.scale.set(0.2);
+    cloud2.x = width / 2 - 180;
+    cloud2.y = height / 2 - 360;
+    cloudsCon.addChild(cloud2);
+
+    let cloud3 = PIXI.Sprite.from("assets/cloud.png");
+    cloud3.scale.set(0.2);
+    cloud3.x = width / 2 - 450;
+    cloud3.y = height / 2 - 250;
+    cloudsCon.addChild(cloud3);
+
+    let cloud4 = PIXI.Sprite.from("assets/cloud.png");
+    cloud4.scale.set(0.2);
+    cloud4.x = width / 2 + 200;
+    cloud4.y = height / 2 - 300;
+    cloudsCon.addChild(cloud4);
+
+    let cloud5 = PIXI.Sprite.from("assets/cloud.png");
+    cloud5.scale.set(0.2);
+    cloud5.x = width / 2 + 400;
+    cloud5.y = height / 2 - 200;
+    cloudsCon.addChild(cloud5);
+    cloudsCon.filters = [displacement];
+
+    // add waves
+    for (let x = 0; x < width + 200; x += 200) {
+      let wave = new PIXI.Graphics();
+      wave.lineStyle(6, 0xffffff);
+      wave.beginFill(0x2c4b8d);
+      wave.drawCircle(x, height / 2 + 180, 100);
+      wave.drawCircle(x, height / 2 + 180, 70);
+      wave.drawCircle(x, height / 2 + 180, 40);
+      wavesCon.addChild(wave);
+
+      gsap.to(wave, {
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2,
+      });
+    }
+
+    for (let x = 50; x < width + 200; x += 200) {
+      let wave = new PIXI.Graphics();
+      wave.lineStyle(6, 0xffffff);
+      wave.beginFill(0x2c4b8d);
+      wave.drawCircle(x, height / 2 + 250, 100);
+      wave.drawCircle(x, height / 2 + 250, 70);
+      wave.drawCircle(x, height / 2 + 250, 40);
+      wavesCon.addChild(wave);
+
+      gsap.to(wave, {
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2,
+        delay: 1,
+      });
+    }
+
+    for (let x = 0; x < width + 200; x += 200) {
+      let wave = new PIXI.Graphics();
+      wave.lineStyle(6, 0xffffff);
+      wave.beginFill(0x2c4b8d);
+      wave.drawCircle(x, height / 2 + 330, 100);
+      wave.drawCircle(x, height / 2 + 330, 70);
+      wave.drawCircle(x, height / 2 + 330, 40);
+      wavesCon.addChild(wave);
+
+      gsap.to(wave, {
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2,
+        delay: 2,
+      });
+    }
+
+    for (let x = 50; x < width + 200; x += 200) {
+      let wave = new PIXI.Graphics();
+      wave.lineStyle(6, 0xffffff);
+      wave.beginFill(0x2c4b8d);
+      wave.drawCircle(x, height / 2 + 400, 100);
+      wave.drawCircle(x, height / 2 + 400, 70);
+      wave.drawCircle(x, height / 2 + 400, 40);
+      wavesCon.addChild(wave);
+
+      gsap.to(wave, {
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2,
+        delay: 1,
+      });
+    }
+
+    for (let x = 0; x < width + 200; x += 200) {
+      let wave = new PIXI.Graphics();
+      wave.lineStyle(6, 0xffffff);
+      wave.beginFill(0x2c4b8d);
+      wave.drawCircle(x, height / 2 + 470, 100);
+      wave.drawCircle(x, height / 2 + 470, 70);
+      wave.drawCircle(x, height / 2 + 470, 40);
+      wavesCon.addChild(wave);
+
+      gsap.to(wave, {
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2,
+        delay: 2,
+      });
+    }
+
+    for (let x = 50; x < width + 200; x += 200) {
+      let wave = new PIXI.Graphics();
+      wave.lineStyle(6, 0xffffff);
+      wave.beginFill(0x2c4b8d);
+      wave.drawCircle(x, height / 2 + 540, 100);
+      wave.drawCircle(x, height / 2 + 540, 70);
+      wave.drawCircle(x, height / 2 + 540, 40);
+      wavesCon.addChild(wave);
+
+      gsap.to(wave, {
+        y: -20,
+        yoyo: true,
+        repeat: -1,
+        duration: 2,
+        delay: 1,
+      });
+    }
+  }
+
+  document.body.appendChild(app.view);
+
+  let context = {};
+
+  app.ticker.add(update, context);
+};
+
+function update(this: any, delta: number) {}
+
+main();
